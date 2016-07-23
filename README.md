5bbc54d90c61557ef143e5c659217318ea920201
malievsky <malievsky@gmail.com>
2016-07-24 00:21:32 +0100

    Add files via upload
    1st commit

--- README.md
+++ README.md
@@ -1,6 +1,63 @@
 # Getting-and-Cleaning-Data-Course-Project
 # README
 
-1 - Merge
+Step0 (preparatory)
+
+It includes data reading + checking for reasult DataFrame dimensions/object sizes (to not overload the RAM)
+Next dataframes created from provided files:
+
+- features, [1] 561  2
+- activityLabels, [1] 6 2
+
+- testSubject, [1] 2947    1
+- testData, [1] 2947  561
+- testActivity, [1] 2947    1
+
+- trainSubject, [1] 7352    1
+- trainData, [1] 7352  561
+- trainActivity, [1] 7352    1
+
+Note: all dataframe sizes get by using of dim comnad as in example below:
+dim(testData) #[1] 2947  561
+object.size(testData) # 13285048 bytes
 
 
+ 
+ Step 1
+The "Subject" and "Activity" values added to "Features" data frame 
+next, the "...Subject" and "...Activity" columns added to both train and test datasets
+neaxt, testData and trainData sets appended
+Appending of Test and Train data sets. Simple
+resulting Data dimension and size:
+ dim(Data) # [1] 10299   563
+ object.size(Data) # 46363336 bytes
+ 
+ 
+ Step2
+ First, function grep used twice to find all appearances of "mean" and "std"
+ strings witih "features":
+ meanCols <- grep("mean", features$V2)
+ meanCols
+ # [1]   1   2   3  41  42  43  81  82  83 121 122 123 161 162 163 201 214 227 240 253 266 267 268 294 295 296 345 346 347 373 374 375
+ # [33] 424 425 426 452 453 454 503 513 516 526 529 539 542 552
+ stdCols <- grep("std", features$V2)
+ stdCols
+ #[1]   4   5   6  44  45  46  84  85  86 124 125 126 164 165 166 202 215 228 241 254 269 270 271 348 349 350 427 428 429 504 517 530
+ #[33] 543
+ 
+ Second, the vectors used to subset Data:
+ meanMeasurements <- Data[, meanCols]
+ dim(meanMeasurements) # [1] 10299    46
+ 
+ Step3
+ function "names" used to assign "variables" dataset  as headers to "Data" frame
+ > tail(names(Data))
+[1] "angle(tBodyGyroJerkMean,gravityMean)" "angle(X,gravityMean)"                 "angle(Y,gravityMean)"                
+[4] "angle(Z,gravityMean)"                 "Subject"                              "Activity"    
+
+ Step4
+ function merge used to add another column to dataset.
+ (from the table "activity_type")
+ 
+ Step5
+ 
---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+# Preparatory step: Data reading, checking for objects dimensons and sizes ####
+
+features <- read.table("./data/UCI HAR Dataset/features.txt", header = FALSE, stringsAsFactors = FALSE)
+activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", header = FALSE, stringsAsFactors = FALSE)
+
+testSubject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", header = FALSE, stringsAsFactors = FALSE)
+testActivity <- read.table("./data/UCI HAR Dataset/test/y_test.txt", header = FALSE, stringsAsFactors = FALSE)
+
+trainSubject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", header = FALSE, stringsAsFactors = FALSE)
+trainActivity <- read.table("./data/UCI HAR Dataset/train/y_train.txt", header = FALSE, stringsAsFactors = FALSE)
+
+testData <- read.table("./data/UCI HAR Dataset/test/X_test.txt", header = FALSE, stringsAsFactors = FALSE)
+object.size(testData) # 13285048 bytes
+
+trainData <- read.table("./data/UCI HAR Dataset/train/X_train.txt", header = FALSE, stringsAsFactors = FALSE)
+object.size(trainData) #33054688 bytes
+
+
+
+
+## Step 1: Merge (append) Datasets #
+# append "Subject" and "Activity" variables to features list:
+tail(features1) # 561 561                 angle(Z,gravityMean)
+features <- rbind(features, c("562", "Subject"))
+features <- rbind(features, c("563", "Activity"))
+
+# rename columns in trainActivity and testActivity sets:
+names(trainActivity)[names(trainActivity)=="V1"] <- "V2"
+names(testActivity)[names(testActivity)=="V1"] <- "V2"
+
+# append "subject" and "activity" columns to main data sets:
+testData <- cbind(testData, testSubject)
+testData <- cbind(testData, testActivity)
+trainData <- cbind(trainData, trainSubject)
+trainData <- cbind(trainData, trainActivity)
+
+Data <- rbind(testData, trainData)
+
+
+## Step 2: Mean & std extracts
+# 2.1 finding and storing of appearances strings "mean" and "std" within the features list:
+meanCols <- grep("mean", features$V2)
+stdCols <- grep("std", features$V2)
+
+# 2.2 extracting (subsetting) of mean columns from Data:
+meanMeasurements <- Data[, meanCols]
+stdMeasurements <- Data[, stdCols]
+
+ 
+## Step 3 dataset columns rename by features list
+names(Data) <- features[, 2]
+
+
+## Step 4 - merging the 
+
+## Step 5:
+
